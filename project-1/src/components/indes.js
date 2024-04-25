c046ebdb07ce3c9bf191274b97b49104a4afdc9c
Pedro Cardoso <pedrohfsc96@gmail.com>
2024-04-25 09:39:29 -0300

    p1

--- project-1/src/components/indes.js
+++
@@ -1,5 +0,0 @@
-export { defaut as Article } from './article/Article'
-export { defaut as Brand } from './Brand/Brand'
-export { defaut as CTA } from './CTA/CTA'
-export { defaut as Feature } from './Feature/Feature'
-export { defaut as Navbar } from './Navbar/Navbar'
