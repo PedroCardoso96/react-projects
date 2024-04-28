c1d15b80b716836c1d90fec0ab48bc5a58105111
Pedro Cardoso <pedrohfsc96@gmail.com>
2024-04-28 15:54:08 -0300

    p1

--- project-1/src/container/possibility/Possibility.jsx
+++ project-1/src/container/possibility/Possibility.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './possibility.css'
 
+import possibilityImage from "../../assets/possibility.png"
+
 const Possibility = () => {
     return (
-        <div>
-            Possibility
+        <div className='gpt3__possibility section__padding' id='possibility'>
+            <div className='gpt3__possibility-image'>
+                <img src={possibilityImage} alt="possibility" />
+            </div>
+            <div className='gpt3__possibility-content'>
+                <h4>Request early access</h4>
+                <h1 className='gradient-text'>
+                    The Possibility are beyond is your imagination.
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa tenetur, laboriosam quas ex voluptas quae nobis libero sit autem. Sed consequatur vel enim quidem deserunt animi nam in provident. Iste?</p>
+                <h4>Request early access</h4>
+            </div>
         </div>
     )
 }
